f90e3fe2c7f732d39884cf80c6e2f12a9bf24750
unknown <saketgupta.ece@gmail.com>
2018-07-17 17:41:00 +0530

    commit3 17july

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,20 @@
 var jQuery =require("jQuery");
+import 'jquery';
 import 'popper.js';
 import 'bootstrap';
 require("../scss/custom.scss");
-
+jquery(document).ready(function(){
+    $.ajax({
+        type: "GET",
+        data: json ,
+        contentType : "application/json",
+        url: 'https://api.themoviedb.org/3/movie/popular?api_key=7520477c96fad381a44633a2b7596a01&language=en-US&page=1',
+        success: function(res){
+            var resData = JSON.stringify(res);
+            //var parseData = jQuery.parseJSON(res);
+            //console.log(resData);
+            //console.log(resData);
+            return resData;
+        },
+    });
+})
